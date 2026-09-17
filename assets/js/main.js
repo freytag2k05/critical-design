2c565d629386be96aa53686d963ea401db34c46f
freytag2k05 <freytag2k05@gmail.com>
2026-09-17 00:04:00 +0000

    cleaned up with prettier

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,14 @@
 // main.js
 
 console.log("Hello, World!");
-let targetElement = document.querySelector("h2")
+let targetElement = document.querySelector("h2");
 console.log(targetElement.outerHTML);
-targetElement.addEventListener("mouseover",function(){
-    console.log(this.innerText);
-    this.innerText="TIME";
-    this.style="cursor:wait";
+targetElement.addEventListener("mouseover", function () {
+  console.log(this.innerText);
+  this.innerText = "TIME";
+  this.style = "cursor:wait";
 });
-targetElement.addEventListener("mouseout",function(){
-    this.innerText="IKEA";
-    this.style="cursor:pointer";
+targetElement.addEventListener("mouseout", function () {
+  this.innerText = "IKEA";
+  this.style = "cursor:pointer";
 });
